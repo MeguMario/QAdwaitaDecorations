54057ea299b084926eaef8dc8c0a4a2abb6dff75
MeguMario <ttrungpt2@gmail.com>
2026-05-12 10:50:02 +0700

    Undo shadow width change

--- src/qadwaitadecorations.cpp
+++ src/qadwaitadecorations.cpp
@@ -48,7 +48,7 @@
 static constexpr int ceButtonSpacing = 12;
 static constexpr int ceButtonWidth = 24;
 static constexpr int ceCornerRadius = 15;
-static constexpr int ceShadowsWidth = 20;
+static constexpr int ceShadowsWidth = 10;
 static constexpr int ceTitlebarHeight = 38;
 static constexpr int ceWindowBorderWidth = 1;
 
